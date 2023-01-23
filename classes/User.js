fd33618c964c1a39d94731ad17616bafaccb1684
Juppsix <julien.postecc@gmail.com>
2023-01-23 11:57:25 +0000

    oui

---
+++ classes/User.js
@@ -0,0 +1,62 @@
+class User {
+    #id; 
+    #username;
+    #email;
+    #password;
+    #firstName;
+    #lastName;
+    #profileImage;
+    
+    constructor(id,username,email,password,firstName,lastName,profileImage) {
+        this.#id = id;
+        this.#username = username;
+        this.#email = email;
+        this.#password = password;
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#profileImage = profileImage;
+    }
+    
+     get id (){
+        return this.#id;
+    }
+    get username (){
+        return this.#username;
+    }
+    get email (){
+        return this.#email;
+    }
+    get password() {
+        return this.#password;
+    }
+    get firstName () {
+        return this.#firstName;
+    }
+    get lastName () {
+        return this.#lastName;
+    }
+    get profileImage () {
+        return this.#profileImage;
+    }
+    set id (id){
+        this.#id = id;
+    }
+    set username (username) {
+        this.#username = username;
+    }
+    set email (email) {
+        this.#email = email;
+    }
+    set password (password) {
+        this.#password = password;
+    }
+    set firstName (firstName) {
+        this.#firstName = firstName;
+    }
+    set lastName (lastName) {
+        this.#lastName = lastName;
+    }
+    set profileImage (profileImage) {
+        this.#profileImage = profileImage;
+    }
+}
